d3fe6df1c30f3543741e2fa5b32f47f214306ce6
Yara Chahine <79076389+YaraChahine@users.noreply.github.com>
2022-02-04 23:26:47 +0400

    final

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,31 +5,81 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText lbp;
-    EditText usd;
+    RadioGroup radioGrp;
+    RadioButton lbp;
+    RadioButton usd;
+    EditText amount;
+    TextView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lbp= (EditText) findViewById(R.id.lbp);
-        usd=(EditText)  findViewById(R.id.usd);
+        radioGrp= (RadioGroup) findViewById(R.id.radioGroup);
+        amount= (EditText)  findViewById(R.id.amount);
+        lbp= (RadioButton) findViewById(R.id.lbp);
+        usd= (RadioButton) findViewById(R.id.usd);
+        result= (TextView) findViewById(R.id.result);
 
     }
 
 
     public void convert(View view){
-        String lbp_input= lbp.getText().toString();
-        String usd_input= usd.getText().toString();
+    if (!amount.getText().toString().isEmpty()) {
+        int amount_res= Integer.parseInt(amount.getText().toString());
+        int amount_final=0;
 
-        if (lbp_input.isEmpty() && usd_input.isEmpty() ){
-            Toast.makeText(getApplicationContext(),"Please enter an amount", Toast.LENGTH_LONG).show();
+        if (radioGrp.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(getApplicationContext(), "Please check a currency", Toast.LENGTH_LONG).show();
+        } else {
+                if(lbp.isChecked()){
+                     amount_final= amount_res*22000;
+                     Integer amnt= new Integer(amount_final);
+                    String res=amnt.toString()+ " L.L";
+                    result.setText(res);
+
+
+                }
+                else
+                {
+                    amount_final= amount_res/22000;
+                    Integer amnt= new Integer(amount_final);
+                    String res=amnt.toString()+ " $";
+                    result.setText(res);
+
+                }
+
+
+
+
+
+            }
         }
-
+    else {
+        Toast.makeText(getApplicationContext(), "Please enter an amount", Toast.LENGTH_LONG).show();
+        result.setText("");
 
     }
-}
+    }
+       /* if (lbp_input.isEmpty() && usd_input.isEmpty() ){
+            Toast.makeText(getApplicationContext(),"Please enter an amount", Toast.LENGTH_LONG).show();
+        }
+        else if (!lbp_input.isEmpty() && !usd_input.isEmpty() ){
+            Toast.makeText(getApplicationContext(),"Please choose one currency.", Toast.LENGTH_LONG).show();
+        }
+        else if (!lbp_input.isEmpty() && usd_input.isEmpty() ) {
+            float lbp_int= (float) Integer.parseInt(lbp_input);
+            float usd_int= lbp_int/22000;
+            lbp.setText();
+        }
+*/
+
+    }

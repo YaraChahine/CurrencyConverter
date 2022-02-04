e6d2b09d4e1bceee042e53cb0f3f48869c89b494
Yara Chahine <79076389+YaraChahine@users.noreply.github.com>
2022-02-04 21:43:16 +0400

    changes

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText lbp;
+    EditText usd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lbp= (EditText) findViewById(R.id.lbp);
+        usd=(EditText)  findViewById(R.id.usd);
+
     }
+
 }
